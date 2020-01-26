f57c2ef512eb31116ab1dca85373ac64a523c74a
Peter Feldgrill <peter.feldgrill@edu.fh-joanneum.at>
2020-01-26 15:07:50 +0100

    Finishing up Home Exercise 2

--- app/src/main/java/at/fh/swengb/feldgrill/moviereview/Movie.kt
+++ app/src/main/java/at/fh/swengb/feldgrill/moviereview/Movie.kt
@@ -13,12 +13,22 @@ open class Movie(
     val reviews: MutableList<Review>)
     {
 
-
+    /*
     fun ratingAverage() : Double{
         var sum = 0.0
         reviews.forEach{
             sum += it.reviewValue
         }
+
         return sum/reviews.count()
-    }
-}
+    }*/
+    fun ratingAverage(): Double {
+        val average = reviews.map { it.reviewValue }.average()
+        if (average.isNaN()) {
+            return 0.0
+                }
+
+                return (Math.round(average *10)/10.0)
+
+            }
+        }
